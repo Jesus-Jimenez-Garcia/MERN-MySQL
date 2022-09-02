3389be7439a16582d5660488ffc4ab1ba91e8db8
Jimy <jimenezmota15@gmail.com>
2022-09-02 19:56:54 +0200

    finished application

--- server/controllers/tasks.controllers.js
+++ server/controllers/tasks.controllers.js
@@ -1,4 +1,4 @@
-import { pool } from "../db.js";
+import { pool } from "../database.js";
 
 export const getTasks = async (req, res) => {
   try {
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
-import * as dotenv from "dotenv"
-dotenv.config({path:"./.env"})
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { PORT } from "./config.js";
 
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,5 +17,7 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
 
-app.listen(process.env.PORT);
-console.log(`Server in running on port ${process.env.PORT}`);
+app.use(express.static(join(__dirname, "../client/dist"))); 
+
+app.listen(PORT);
+console.log(`Server in running on port ${PORT}`);
